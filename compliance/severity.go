7213395de19a789e3eabd6c98645b8f3715aa82a
Máximo Cuadros <mcuadros@gmail.com>
2019-06-30 00:10:09 +0200

    compliance: Severity.Color and tests
    Signed-off-by: Máximo Cuadros <mcuadros@gmail.com>

---
+++ compliance/severity.go
@@ -0,0 +1,75 @@
+package compliance
+
+import (
+	"fmt"
+
+	"github.com/fatih/color"
+)
+
+// Severity describes the severity of a rule.
+type Severity int
+
+const (
+	_ Severity = iota
+	Low
+	Medium
+	High
+	Critical
+)
+
+// Color returns a color.Color representing the severity.
+func (s Severity) Color() *color.Color {
+	switch s {
+	case Low:
+		return color.New(color.FgCyan)
+	case Medium:
+		return color.New(color.FgYellow)
+	case High:
+		return color.New(color.FgRed)
+	case Critical:
+		return color.New(color.FgHiRed).Add(color.BgWhite)
+	default:
+		return color.New(color.FgWhite)
+	}
+}
+
+func (s Severity) String() string {
+	switch s {
+	case Low:
+		return "LOW"
+	case Medium:
+		return "MEDIUM"
+	case High:
+		return "HIGH"
+	case Critical:
+		return "CRITITCAL"
+	default:
+		return fmt.Sprintf("UNKNOWN(%d)", s)
+	}
+}
+
+//UnmarshalYAML honors the yaml.Unmarshaler interface.
+func (s *Severity) UnmarshalYAML(unmarshal func(interface{}) error) error {
+	var str string
+	err := unmarshal(&str)
+	if err != nil {
+		return err
+	}
+
+	switch str {
+	case "low":
+		*s = Low
+		return nil
+	case "medium":
+		*s = Medium
+		return nil
+	case "high":
+		*s = High
+		return nil
+	case "critical":
+		*s = Critical
+		return nil
+	default:
+		return fmt.Errorf("invalid severity value %q", str)
+	}
+}
---
+++ compliance/severity_test.go
@@ -0,0 +1,47 @@
+package compliance
+
+import (
+	"testing"
+
+	"gopkg.in/yaml.v2"
+
+	"github.com/fatih/color"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestSeverityColor(t *testing.T) {
+	for i := Low - 1; i <= Critical; i++ {
+		assert.IsType(t, &color.Color{}, i.Color())
+	}
+}
+
+func TestSeverityString(t *testing.T) {
+	for i := Low - 1; i <= Critical; i++ {
+		assert.IsType(t, "", i.String())
+	}
+}
+
+const severityYAML = `
+low: low
+high: high
+medium: medium
+critical: critical
+`
+
+func TestSeverityUnmarshalYAML(t *testing.T) {
+	out := map[string]Severity{}
+	err := yaml.Unmarshal([]byte(severityYAML), out)
+	assert.NoError(t, err)
+
+	assert.Len(t, out, 4)
+	assert.Equal(t, Low, out["low"])
+	assert.Equal(t, Medium, out["medium"])
+	assert.Equal(t, High, out["high"])
+	assert.Equal(t, Critical, out["critical"])
+}
+
+func TestSeverityUnmarshalYAML_Error(t *testing.T) {
+	out := map[string]Severity{}
+	err := yaml.Unmarshal([]byte("err: no-defined"), out)
+	assert.Error(t, err)
+}
